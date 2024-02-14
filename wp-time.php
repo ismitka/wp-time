1f0324ad04cddf9b50f1fc420c4ac49b3b72384a
Ivan Smitka <ivan@smitka.net>
2024-02-14 17:07:52 +0100

    Update Site

--- wp-time.php
+++ wp-time.php
@@ -2,6 +2,7 @@
 /*
  * Plugin Name: WP-Time
  * Plugin URI: https://www.smitka.net/wp-time
+ * Update URI: https://www.smitka.net/wp-plugin/wp-time
  * Description: JavaScript init elements by Time
  * Version: 1.0
  * Author: Ivan Smitka
@@ -34,7 +35,11 @@
 
 class WP_Time {
 
+	const VERSION = "1.0";
+
 	const CRON_REGEXP = "/^(\*|[0-9,\-\/\*]+)\s+(\*|[0-9,\-\/\*]+)\s+(\*|[0-9,\-\/\*]+)\s+(\*|[0-9,\-\/\*]+)\s+(\*|[0-9,\-\/\*]+)(\s+(\*|[0-9,\-\/\*]+))?$/";
+
+	const UPDATE_HOST = "https://smitka.ismnb.smitka.net";
 
 	public static function init() {
 		// Scripts
@@ -45,7 +50,67 @@ class WP_Time {
 			] );
 			add_shortcode( 'wp-time', [ 'WP_Time', 'html_current' ] );
 			add_shortcode( 'wp-time-on', [ 'WP_Time', 'html_on' ] );
+		} else {
+			add_filter( 'update_plugins_www.smitka.net', function ( $update, $plugin_data, $plugin_file, $locales ) {
+				/*
+				This filter is applied inside a foreach loop in wp_update_plugins().
+				So, if there a several plugins using the same hostname as Update URI, our function will be run for each of those other plugins.
+				Better check if the loop has reached *our* plugin until we do anything.
+				 */
+				if ( $plugin_file == plugin_basename( __FILE__ ) ) {
+					$request      = wp_remote_get( $plugin_data['UpdateURI'] );
+					$request_body = wp_remote_retrieve_body( $request );
+					$update       = json_decode( $request_body, true );
+				}
+
+				return $update;
+			}, 10, 4 );
 		}
+	}
+
+	public function update( $transient ) {
+		if ( empty( $transient->checked ) ) {
+			return $transient;
+		}
+
+		$remote = wp_remote_get(
+			self::UPDATE_HOST . "/wp-plugin/wp-time",
+			array(
+				'timeout' => 10,
+				'headers' => array(
+					'Accept' => 'application/json'
+				)
+			)
+		);
+
+		if ( is_wp_error( $remote )
+		     || wp_remote_retrieve_response_code( $remote ) !== 200
+		     || empty( wp_remote_retrieve_body( $remote ) )
+		) {
+			return $transient;
+		}
+
+		$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+
+		// your installed plugin version should be on the line below! You can obtain it dynamically of course
+		if ( $remote
+		     && version_compare( self::VERSION, $remote->version, '<' )
+		     && version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
+		     && version_compare( $remote->requires_php, PHP_VERSION, '<' )
+		) {
+
+			$res                                 = new stdClass();
+			$res->slug                           = $remote->slug;
+			$res->plugin                         = plugin_basename( __FILE__ ); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
+			$res->new_version                    = $remote->version;
+			$res->tested                         = $remote->tested;
+			$res->package                        = $remote->download_url;
+			$transient->response[ $res->plugin ] = $res;
+
+			//$transient->checked[$res->plugin] = $remote->version;
+		}
+
+		return $transient;
 	}
 
 	public static function enqueue_scripts() {
@@ -87,7 +152,7 @@ class WP_Time {
 	}
 }
 
-add_action( 'plugins_loaded', array(
+add_action( 'plugins_loaded', [
 	'WP_Time',
 	'init'
-), 100 );
+], 100 );
