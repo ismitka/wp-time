ca32785e3baff3aa3825d0b33e363fe2fc009584
Ivan Smitka <ivan@smitka.net>
2025-07-19 17:16:03 +0200

    Version Management

--- compress.sh
+++ compress.sh
@@ -1,4 +1,9 @@
-(cd ../ && zip -r wp-time/wp-time.zip \
+#!/bin/bash
+sass scss/default.scss static/css/default.css
+VERSION=$(git tag -l | sort -r | head -1)
+echo "${VERSION}"
+sed -i -E "s/^ \* Version\: .*$/ * Version: ${VERSION}/g" wp-time.php
+(cd ../ && zip -r wp-time/${VERSION}.zip \
   wp-time/index.html \
   wp-time/LICENSE \
   wp-time/README.md \
